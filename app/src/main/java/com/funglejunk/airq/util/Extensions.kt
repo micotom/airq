325121fcec654a6310b3fdaa29a6d35fab4e37bc
mmarth <michael.marth@bragi.com>
2018-05-13 15:52:20 +0200

    - removed unused property - added simple fold extension function to simply copy failure in new parameterized try

--- app/src/main/java/com/funglejunk/airq/util/Extensions.kt
+++ app/src/main/java/com/funglejunk/airq/util/Extensions.kt
@@ -9,17 +9,6 @@ import io.reactivex.functions.BiFunction
 import timber.log.Timber
 import java.math.BigDecimal
 
-class Extensions {
-
-    class String {
-        companion object {
-            @JvmField
-            val Empty: kotlin.String = ""
-        }
-    }
-
-}
-
 fun <T: Any, V: Any> zipToPair(s1: Single<T>, s2: Single<V>): Single<Pair<T, V>> {
     return s1.zipWith(s2, BiFunction { o1Value, o2Value -> Pair(o1Value, o2Value) })
 }
@@ -44,4 +33,11 @@ fun Pair<Response, Result<String, FuelError>>.mapToTry(): Try<String> {
         is Result.Failure -> Try.Failure(result.error)
         is Result.Success -> Try.Success(result.value)
     }
+}
+
+inline fun <T, V> Try<T>.simpleFold(onSuccess: (T) -> Try<V>): Try<V> {
+    return fold(
+            { Try.Failure(it) },
+            { onSuccess(it) }
+    )
 }
